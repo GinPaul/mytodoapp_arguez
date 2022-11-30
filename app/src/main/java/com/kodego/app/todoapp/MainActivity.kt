4f29d8e743497bb00fba731784ac4e777a68ed4b
GinPaul <luginlpp@gmail.com>
2022-11-30 10:58:50 +0800

    Nov 30 2022 commit 10.58AM

--- app/src/main/java/com/kodego/app/todoapp/MainActivity.kt
+++ app/src/main/java/com/kodego/app/todoapp/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.kodego.app.todoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.kodego.app.todoapp.databinding.ActivityMainBinding
 import com.kodego.app.todoapp.db.TodoApplication
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity(), TaskItemClickListener
 {
+    lateinit var simpleDateFormat: SimpleDateFormat
+    lateinit var date: String
+    lateinit var textView: TextView
+    lateinit var calendar: Calendar
+
     private lateinit var binding: ActivityMainBinding
     //private lateinit var taskViewModel: TaskViewModel
     private val taskViewModel: TaskViewModel by viewModels {
@@ -26,6 +36,13 @@ class MainActivity : AppCompatActivity(), TaskItemClickListener
             NewTaskSheet(null).show(supportFragmentManager, "newTaskTag")
         }
         setRecyclerView()
+
+        //showing current date and time
+        textView = binding.tvDateToday
+        calendar = Calendar.getInstance()
+        simpleDateFormat = SimpleDateFormat("EEE | MMM dd, yyyy")
+        date = simpleDateFormat.format(calendar.time)
+        textView.text = date
     }
 
     private fun setRecyclerView()
@@ -48,4 +65,16 @@ class MainActivity : AppCompatActivity(), TaskItemClickListener
     {
         taskViewModel.setCompleted(taskItem)
     }
+
+//    //for date and time today
+//    fun addCalendarEvent(view: View) {
+//        val calendarEvent: Calendar = Calendar.getInstance()
+//        val intent = Intent(Intent.ACTION_EDIT)
+//        intent.type = "vnd.android.cursor.item/event"
+//        intent.putExtra("beginTime", calendarEvent.timeInMillis)
+//        intent.putExtra("allDay", true)
+//        intent.putExtra("rule", "FREQ=YEARLY")
+//        intent.putExtra("endTime", calendarEvent.timeInMillis + 60 * 60 * 1000)
+//        intent.putExtra("title", "Calendar Event")
+//        startActivity(intent)
 }
